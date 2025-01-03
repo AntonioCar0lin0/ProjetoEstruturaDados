4688a5a2b1e54ec7f59dbacc800bc6277c6cd53e
Luiza Trigueiro <lulu.study.2005@gmail.com>
2025-01-03 20:45:12 -0300

    Criação dos atributos iniciais de Eleven

--- Personagens/Eleven.h
+++ Personagens/Eleven.h
@@ -5,4 +5,13 @@
 #ifndef ELEVEN_H
 #define ELEVEN_H
 
+#include "Personagem.h"
+
+class Eleven : public Personagem {
+public:
+    Eleven()
+        : Personagem("Eleven", 100, 10, 7, 10, 6, 9) {
+    }
+};
+
 #endif //ELEVEN_H

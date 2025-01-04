7291ae877ed54bf7d5737a28333ff4af17655e84
Luiza Trigueiro <lulu.study.2005@gmail.com>
2025-01-03 21:25:21 -0300

    Criando primeiros atributos de Will

--- Personagens/Will.h
+++ Personagens/Will.h
@@ -5,4 +5,13 @@
 #ifndef WILL_H
 #define WILL_H
 
+#include "Personagem.h"
+
+class Will : public Personagem {
+public:
+    Will()
+        : Personagem("Will", 100, 2, 8, 6, 7, 10) {
+    }
+};
+
 #endif //WILL_H

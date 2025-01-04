3563c128175dfc18325aa6f0e45452e4c281829d
anton <antoniorpcarolino@gmail.com>
2025-01-04 20:01:58 -0300

    Criando os atributos iniciais da personagem max

--- Personagens/Max.h
+++ Personagens/Max.h
@@ -5,4 +5,11 @@
 #ifndef MAX_H
 #define MAX_H
 
+#include <Personagem.h>
+
+class Max : public Personagem {
+    public:
+    Max()
+        : Personagem("Max", 100, 4, 9, 8, 7, 8 ) {};
+};
 #endif //MAX_H

4e97ec173e956b1f137d13b8631f9daa008f7cad
anton <antoniorpcarolino@gmail.com>
2025-01-05 14:59:30 -0300

    feat: criando os atributos inicias dos vilões

--- Personagens/Demodog.h
+++ Personagens/Demodog.h
@@ -4,5 +4,10 @@
 
 #ifndef DEMODOG_H
 #define DEMODOG_H
-
+#include <Personagem.h>
+class Demodog : public Personagem {
+    public:
+    Demodog()
+        : Personagem("Demodog", 50, 7, 5, 8, 3, 7){}
+};
 #endif //DEMODOG_H
--- Personagens/DevoradorDeMentes.h
+++ Personagens/DevoradorDeMentes.h
@@ -5,6 +5,12 @@
 #ifndef DEVORADORDEMENTES_H
 #define DEVORADORDEMENTES_H
 
+#include <Personagem.h>
+class DevoradorDeMentes : public Personagem{
+    public:
+    DevoradorDeMentes()
+        : Personagem("Devorador de Mentes", 150, 9, 8, 8, 0, 10){}
+};
 
 
 

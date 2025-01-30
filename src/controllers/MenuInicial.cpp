9a125f095e80a9cd4e00be73cd5986bfbd19821a
Luiza Trigueiro <lulu.study.2005@gmail.com>
2025-01-30 14:16:03 -0300

    Ajustes feitos nos Rounds, conserto de bugs

--- src/controllers/MenuInicial.cpp
+++ src/controllers/MenuInicial.cpp
@@ -61,8 +61,8 @@ void MenuInicial::exibirMenuInicial() {
     std::wcout <<L"*************[APRESENTAÇÃO DO JOGO]************"<< std::endl;
     std::wcout <<L"***********************************************"<< std::endl;
     std::wcout << L"Hawkins está à beira de um novo colapso sobrenatural, mais sombrio e devastador do que qualquer coisa enfrentada antes. Uma fissura antiga, chamada O Abismo, começa a se abrir no coração da 'Floresta das Trevas', uma conexão entre o mundo  real, o Mundo Invertido e algo ainda mais primitivo e maligno."  << std::endl;
-    std::wcout << L"Enquanto fenômenos sobrenaturais se espalham, sinais da presença de antigos inimigos, como o Devorador de Mentes. Desta vez, ele não está sozinho. Vecna, o arquiteto de todo o mal causado em Hawkins, ressurgiu, mais  poderoso, e se tornou o supremo guardião do Abismo, determinado a trazê-lo à totalidade." << std::endl;
-    std::wcout <<L"Juntando-se às outras diversas criaturas do Mundo Invertido: Demogorgons, Demodogs e os letais Demobats, que patrulham o céu e atacam qualquer intruso que se  aproxime." << std::endl;
+    std::wcout << L"Enquanto fenômenos sobrenaturais se espalham, sinais da presença de antigos inimigos, como o Devorador de Mentes. Desta vez, ele não está sozinho. Vecna, o arquiteto de todo o mal causado em Hawkins, ressurgiu, mais  poderoso, e se tornou o supremo guardião do Abismo, determinado a trazê-lo à totalidade," << std::endl;
+    std::wcout <<L"juntamente às outras diversas criaturas do Mundo Invertido: Demogorgons, Demodogs e os letais Demobats. O seu desafio é coletar os quatro Selos do Abismo -partes do Vecna espalhadas por Hawkins- e destruí-los para salvar a cidade desse mal! " << std::endl;
     do {
         std::wcout << L"\nEscolha uma opção:" << std::endl;
         std::wcout << L"1. Iniciar o jogo" << std::endl;
@@ -74,9 +74,7 @@ void MenuInicial::exibirMenuInicial() {
 
         switch(opcao) {
             case 1:
-                //IniciarJogo iniciarJogo;
                 iniciarJogo.exibirMenuIniciarJogo();
-
                 break;
             case 2:
                 exibirMenuInstrucoesGerais();
@@ -93,15 +91,3 @@ void MenuInicial::exibirMenuInicial() {
     } while (opcao != 3);
 }
 
-// main para ver se a aplicação individualmente tá rodando
-//int main() {
-//    _setmode(_fileno(stdout), _O_U8TEXT);
-//    setlocale(LC_ALL, "");
-//
-    // Chama o menu inicial
-//    MenuInicial menuInicial;
-//    menuInicial.exibirMenuInicial();
-
-//    return 0;
-//}
-//
